114315e1c40340fc86426640eb8c2601d18abfd4
Sugey SL. Lopez <Sugey@IntegraLab.local>
2016-08-19 15:11:21 -0600

    lenar combos

---
+++ app/src/main/java/com/example/sugey/seminario/Consultas.java
@@ -0,0 +1,58 @@
+package com.example.sugey.seminario;
+
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ * Created by Sugey on 19/08/2016.
+ */
+
+public class Consultas {
+    private String[] datos;
+    Conexion Conexion;
+    PreparedStatement stmt;
+    ResultSet rs;
+    private Spinner cmbAutores;
+
+    public ArrayList<String> buscarAutores(){
+
+        Conexion = new Conexion();
+        Connection con = Conexion.CONN();
+        ArrayList<String> data2 = new ArrayList<String>();
+        data2.add(0,"Seleccionar Autor..");
+
+        if (con == null) {
+
+
+        }else{
+            try {
+                //instruccion
+
+                String query = "select nombre from BCatAutores";
+                stmt = con.prepareStatement(query);
+                rs = stmt.executeQuery();
+
+
+                while (rs.next()) {
+                    String id2 = rs.getString("nombre");
+                    data2.add(id2);
+                }
+                String[] array2 = data2.toArray(new String[0]);
+
+
+            }catch(Exception ex){
+
+
+
+            }
+
+        }
+        return data2;
+    }
+}
--- app/src/main/java/com/example/sugey/seminario/MainActivity.java
+++ app/src/main/java/com/example/sugey/seminario/MainActivity.java
@@ -1,45 +1,55 @@
 package com.example.sugey.seminario;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnGuardar;
-    Conexion Conexion;
-    private String[] datos;
+
+
+    private Spinner cmbAutores;
+    Consultas consulta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //metodo para llenar los combos
+        //inicia clase que contiene todas las consultas
+         consulta = new Consultas();
+        //se inician lementos de la actividad que se usaran
+         cmbAutores = (Spinner)findViewById(R.id.cmbautor);
 
-
-        Conexion = new Conexion();
-        btnGuardar = (Button)findViewById(R.id.btnGuardar);
-        btnGuardar.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 Connection con = Conexion.CONN();
-                 if (con == null) {
-                     Toast.makeText(getApplicationContext(),
-                             "Sin Conexion a Base de Datos",
-                             Toast.LENGTH_LONG).show();
-                 }
-             }
-         });
-
+       //se crea un adaptador con los datos resultantes del metodo de la consulta contenida en consultas - se llena el combo
+          ArrayAdapter NoCoreAdapter2 = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, consulta.buscarAutores());
+          cmbAutores.setAdapter(NoCoreAdapter2);
 
     }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
 
 
 }
